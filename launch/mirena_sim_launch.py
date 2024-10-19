c4855f08813c645c6d45e3f4046e2b6701ebf585
Ozuba <miguelozubas@gmail.com>
2024-10-19 15:58:04 +0200

    New launch system

---
+++ launch/mirena_sim_launch.py
@@ -0,0 +1,18 @@
+import launch
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
+import shutil
+
+def generate_launch_description():
+    # Get the path to the ROS 2 package
+    package_path = get_package_share_directory('mirena_sim')  # Replace with your package name
+    godot_project_path = os.path.join(package_path, 'MirenaSim')  # Relative path to Godot project
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['godot', '--path', godot_project_path],  # Command to run Godot with the project path
+            output='screen'  # Display output in the terminal
+        ),
+    ])
