9c35e93064a47b9eb283f2d8b712237457acb570
colmenasio <a.colmenero.fernandez@gmail.com>
2024-10-23 19:40:08 +0200

    Added position publishing

--- src/mirena_car.hpp
+++ src/mirena_car.hpp
@@ -8,6 +8,8 @@
 //ROS
 #include "rclcpp/rclcpp.hpp"
 #include "mirena_msg/msg/car_controls.hpp"  
+#include "mirena_msg/msg/car_position.hpp"
+#include "mirena_msg/msg/car_acceleration.hpp"  
 
 //OTHER
 #include "custom_macros.hpp"
@@ -19,7 +21,9 @@ class MirenaCar : public Node {
 private:
     //ROS subscriber and callback
 	rclcpp::Node::SharedPtr rosNode;
-    rclcpp::Subscription<mirena_msg::msg::CarControls>::SharedPtr rosSub;
+    rclcpp::Subscription<mirena_msg::msg::CarControls>::SharedPtr rosControlsSub;
+	rclcpp::Publisher<mirena_msg::msg::CarPosition>::SharedPtr rosPositionPub;
+	rclcpp::Publisher<mirena_msg::msg::CarAcceleration>::SharedPtr rosAccelerationPub;
     // Callback function for when a message is received
 
     // Subscription object
@@ -30,6 +34,11 @@ private:
 	uint8_t gas;
 	uint8_t brake;
 	double steer_angle;
+
+	//Car position Outputs
+	double x_pos;
+	double y_pos;
+	double car_angle;
 
 protected:
 	static void _bind_methods();
@@ -45,12 +54,17 @@ public:
 	uint8_t get_brake();
 	void set_steer_angle(double _steer_angle);
 	double get_steer_angle();
-	void set_x_position();
+	void set_x_pos(double _x_pos);
+	void set_y_pos(double _y_pos);
+	void set_car_angle(double _car_angle);	
+
 	//Godot
 	void _process(double delta) override;
 
 	//ROS
-	void topic_callback(const mirena_msg::msg::CarControls::SharedPtr msg);
+	void control_sub_callback(const mirena_msg::msg::CarControls::SharedPtr msg);
+	void publish_position();
+	void publish_acceleration(double delta);
 
 };
 
