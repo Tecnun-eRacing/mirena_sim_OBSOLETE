35b456404bf2f5d1589861f873290a9435113652
Ozuba <miguelozubas@gmail.com>
2024-10-23 16:04:54 +0200

    Added publish rate to lidar, improved performance

--- src/mirena_lidar.hpp
+++ src/mirena_lidar.hpp
@@ -6,55 +6,67 @@
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <omp.h>
 
-
-//ROS
+// ROS
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
-namespace godot {
+namespace godot
+{
 
-class MirenaLidar : public Node3D {
-    GDCLASS(MirenaLidar, Node3D)
+    class MirenaLidar : public Node3D
+    {
+        GDCLASS(MirenaLidar, Node3D)
 
-private:
-    //ROS publisher
-    rclcpp::Node::SharedPtr node;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub;
+    private:
+        // ROS publisher
+        rclcpp::Node::SharedPtr node;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub;
 
-    double max_range;
-    int horizontal_resolution;
-    int vertical_resolution;
-    double vertical_fov;
-    double horizontal_fov;
-    uint32_t collision_mask;
+        double max_range;
+        int horizontal_resolution;
+        int vertical_resolution;
+        double vertical_fov;
+        double horizontal_fov;
+        uint32_t collision_mask;
+        //Rate
+        double publish_rate;
+        double last_publish_time;
 
-protected:
-    static void _bind_methods();
+    protected:
+        static void _bind_methods();
 
-public:
-    MirenaLidar();
-    ~MirenaLidar();
+    public:
+        MirenaLidar();
+        ~MirenaLidar();
 
-    void set_max_range(double p_range);
-    double get_max_range() const;
+		void _process(double delta) override;
 
-    void set_horizontal_resolution(int p_resolution);
-    int get_horizontal_resolution() const;
 
-    void set_vertical_resolution(int p_resolution);
-    int get_vertical_resolution() const;
 
-    void set_vertical_fov(double p_fov);
-    double get_vertical_fov() const;
+        //Getters and Setters
+        void set_publish_rate(double p_rate);
+        double get_publish_rate() const;
 
-    void set_horizontal_fov(double p_fov);
-    double get_horizontal_fov() const;
+        void set_max_range(double p_range);
+        double get_max_range() const;
 
-    void set_collision_mask(uint32_t p_mask);
-    uint32_t get_collision_mask() const;
+        void set_horizontal_resolution(int p_resolution);
+        int get_horizontal_resolution() const;
 
-    void scan();
-};
+        void set_vertical_resolution(int p_resolution);
+        int get_vertical_resolution() const;
+
+        void set_vertical_fov(double p_fov);
+        double get_vertical_fov() const;
+
+        void set_horizontal_fov(double p_fov);
+        double get_horizontal_fov() const;
+
+        void set_collision_mask(uint32_t p_mask);
+        uint32_t get_collision_mask() const;
+
+        void scan();
+    };
 
 }
 
