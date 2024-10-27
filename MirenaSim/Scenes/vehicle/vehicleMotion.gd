0c3a73ae5ccb8e9a2977332da2a14ba5ae1234d0
Ozuba <miguelozubas@gmail.com>
2024-10-27 16:01:51 +0100

    General improvements in vehicle model and simulator

--- MirenaSim/Scenes/vehicle/vehicleMotion.gd
+++ MirenaSim/Scenes/vehicle/vehicleMotion.gd
@@ -1,7 +1,7 @@
 extends VehicleBody3D
 
 const ENGINE_F = 900
-
+const BRAKE_F = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,6 +11,7 @@ func _ready():
 func _process(delta):
 	steering = $MirenaCar.steer_angle
 	engine_force = $MirenaCar.gas*ENGINE_F/255
+	brake = $MirenaCar.brake * BRAKE_F/255
 	$MirenaLidar.publish_rate = 5.0
 	$MirenaCam.publish_rate = 30.0
 	$MirenaImu.publish_rate = 30.0
