6c8e0c0ec627881922e8f104a7343dd737deaf96
Ozuba <miguelozubas@gmail.com>
2024-11-16 00:55:32 +0100

    Returned to old launch system deprecated project export for simplicity

--- launch/mirena_sim_launch.py
+++ launch/mirena_sim_launch.py
@@ -7,11 +7,12 @@ import shutil
 
 def generate_launch_description():
     # Get the path to the ROS 2 package
-    package_path = get_package_share_directory('mirena_sim')  # Get theshare install folder
-    godot_project_path = os.path.join(package_path, 'MirenaSim/MirenaSim')  # Relative path to sim executable
+    package_path = get_package_share_directory('mirena_sim')  # Replace with your package name
+    godot_project_path = os.path.join(package_path, 'MirenaSim')  # Relative path to Godot project
+
     return LaunchDescription([
         ExecuteProcess(
-            cmd=[godot_project_path],  # Command to run Godot with the project path
+            cmd=['godot','--path', godot_project_path],  # Command to run Godot with the project path
             output='screen'  # Display output in the terminal
         ),
     ])
