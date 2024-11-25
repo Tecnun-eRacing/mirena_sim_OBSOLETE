9c255a0beb614d88a6974dd2847f8878b40c22bd
Ozuba <miguel.oroz@zubasti.net>
2024-11-25 01:50:49 +0100

    Added Improved track loading with spawn position

---
+++ MirenaSim/Scenes/sim_env.gd
@@ -0,0 +1,57 @@
+extends Node
+
+#Eliminates all members of the group cones
+func clearCones():
+	for cone in get_tree().get_nodes_in_group("Cones"):
+		cone.queue_free() # Delete each element
+
+func loadTrack(path : String):
+	clearCones() # Clears existing track
+	var file = FileAccess.open(path, FileAccess.READ)
+	var data = file.get_as_text()
+	var json = JSON.new()
+	json.parse(data)
+	var track = json.data
+	for cone in track["cones"]:
+		var model
+		match cone["type"]:
+			"blue":
+				model = load("res://Models/Cone/bCone.glb").instantiate() 
+			"yellow":
+				model = load("res://Models/Cone/yCone.glb").instantiate() 
+		model.translate(Vector3(cone["x"],0,cone["y"]))
+		$Track.add_child(model)
+		model.add_to_group("Cones")
+		# Move car to spawnPos
+	print("Spawn Pos",Vector3(track["spawn"]["x"],0,track["spawn"]["y"]))
+	#$vehicle.freeze = true
+	#await get_tree().process_frame # Hacemos que el freeze tenga efecto
+	#$vehicle.translate(Vector3(track["spawn"]["x"],0,track["spawn"]["y"]))
+	#$vehicle.freeze = false
+	PhysicsServer3D.body_set_state(
+	$vehicle.get_rid(),
+	PhysicsServer3D.BODY_STATE_TRANSFORM,
+	Transform3D.IDENTITY.rotated(Vector3.UP, track["spawn"]["theta"])
+	.translated(Vector3(track["spawn"]["x"],0,track["spawn"]["y"]))
+	
+	)
+
+
+	
+func _on_file_selected(path: String) -> void:
+	print("Selected track path: ", path)
+	loadTrack(path)
+	
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Check if the "m" key is pressed
+	if Input.is_action_just_pressed("load_track"):
+		# Show the FileDialog
+		$FileDialog.popup_centered()
+	
--- MirenaSim/Scripts/track.gd
+++ MirenaSim/Scripts/track.gd
@@ -6,37 +6,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	 # Check if the "m" key is pressed
-	if Input.is_action_just_pressed("load_track"):
-		# Show the FileDialog
-		$FileDialog.popup_centered()
 	pass
-
-#Eliminates all members of the group cones
-func clearCones():
-	for cone in get_tree().get_nodes_in_group("Cones"):
-		cone.queue_free() # Delete each element
-		
-func loadCones(path : String):
-	clearCones() # Clears existing track
-	var file = FileAccess.open(path, FileAccess.READ)
-	var data = file.get_as_text()
-	var json = JSON.new()
-	json.parse(data)
-	var track = json.data
-	for cone in track["cones"]:
-		var model
-		match cone["type"]:
-			"blue":
-				model = load("res://Models/Cone/bCone.glb").instantiate() 
-			"yellow":
-				model = load("res://Models/Cone/yCone.glb").instantiate() 
-		model.translate(Vector3(cone["x"],0,cone["y"]))
-		add_child(model)
-		model.add_to_group("Cones")
-
-
-func _on_file_selected(path: String) -> void:
-	print("Selected track path: ", path)
-	loadCones(path)
-	
