01759c29037b349bd4a8076219991bbc60072b5a
Ozuba <miguel.oroz@zubasti.net>
2024-12-05 02:13:42 +0100

    Chore: File Cleanup

--- launch/mirena_sim_launch.py
+++ launch/mirena_sim_launch.py
@@ -1,18 +1,22 @@
 import launch
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch_ros.actions import PushROSNamespace
 from ament_index_python.packages import get_package_share_directory
 import os
-import shutil
+
 
 def generate_launch_description():
     # Get the path to the ROS 2 package
-    package_path = get_package_share_directory('mirena_sim')  # Replace with your package name
-    godot_project_path = os.path.join(package_path, 'MirenaSim')  # Relative path to Godot project
+    package_path = get_package_share_directory(
+        'mirena_sim')  # Replace with your package name
+    # Relative path to Godot project
+    godot_project_path = os.path.join(package_path, 'MirenaSim')
 
     return LaunchDescription([
         ExecuteProcess(
-            cmd=['godot','--path', godot_project_path],  # Command to run Godot with the project path
-            output='screen'  # Display output in the terminal
+            # Command to run Godot with the project path
+            cmd=['godot', '--path', godot_project_path],
+            output='screen',  # Display output in the terminal
         ),
     ])
