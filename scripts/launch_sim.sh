74741147436773c45cea142ada134faf4bd1ace4
Ozuba <miguel.oroz@zubasti.net>
2025-03-15 16:50:41 +0100

    Improved overall experience, bundling x86 godot executable and an improved launch system using ros2 run with bash scripts

---
+++ scripts/launch_sim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Get the directory of the script (where the executable is)
+DIR=$(dirname "$(realpath "$0")")
+
+# Set the working directory to where the Godot project is located (install/share/mirena_sim)
+PROJECT_DIR="../../share/mirena_sim/MirenaSim"
+
+# Get into this directory to launch godot
+cd "$DIR"
+
+# Run the Godot executable with the project path
+./godot --path "$PROJECT_DIR" "$@" # Replace with your actual executable if needed
